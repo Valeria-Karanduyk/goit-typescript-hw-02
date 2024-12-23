2816eae67b4a843af39f3bb100e5644d10599a22
Valeria_Karanduyk <lera-ka0195@ukr.net>
2024-12-23 15:49:58 +0200

    add image modal

---
+++ src/components/ImageModal/ImageModal.tsx
@@ -0,0 +1,76 @@
+import s from "./ImageModal.module.css";
+import Modal from "react-modal";
+import { FaRegHeart, FaWindowClose } from "react-icons/fa";
+import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
+import { useEffect } from "react";
+import { Image } from "../App/App.types";
+
+type Props = {
+  isOpen: boolean;
+  image: Image | null;
+  onClose: () => void;
+}
+const ImageModal = ({ isOpen, onClose, image }: Props) => {
+  useEffect(() => {
+    if (isOpen) {
+      disableBodyScroll(document.body);
+    } else {
+      enableBodyScroll(document.body);
+    }
+  }, [isOpen]);
+  return (
+    <Modal
+      overlayClassName={s.overlay}
+      className={s.content}
+      isOpen={isOpen}
+      onRequestClose={onClose}
+    >
+      <button className={s.btn} onClick={onClose}>
+        <FaWindowClose className={s.icon} />
+      </button>
+      {image && (
+        <div className={s.container}>
+          <div className={s.imgContainer}>
+            <img
+              className={s.img}
+              src={image.urls.regular}
+              alt={image.alt_description}
+            />
+          </div>
+          <div className={s.moreInform}>
+            <p className={s.author}>
+              Author:{" "}
+              <a
+                className={s.linkAuthor}
+                href={image.user.social.portfolio_url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {image.user.name}
+              </a>
+            </p>
+            <p className={s.likes}>
+            <FaRegHeart className={s.iconLikes} />
+              Likes: <span className={s.likesSpan}>{image.likes}</span>
+            </p>
+            {image.description && (
+              <p className={s.description}>{image.description}</p>
+            )}
+            <ul className={s.tagsList}>
+              {image.tags.map((tag: { title: string }, index: number) => (
+                <li className={s.tagItem} key={index}>
+                  &#35;{tag.title}
+                </li>
+              ))}
+            </ul>
+            {image.user.location && (
+              <p className={s.location}>Location: {image.user.location}</p>
+            )}
+          </div>
+        </div>
+      )}
+    </Modal>
+  );
+};
+
+export default ImageModal;
